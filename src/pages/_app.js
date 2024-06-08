cd8a7409cf7f4437934da6320c7f11ea566849b8
gabeferreiraa <146259686+gabeferreiraa@users.noreply.github.com>
2024-06-08 16:14:30 -0400

    added functionality to add org, remove org, working on adding to local storage

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,20 @@
 import "@/styles/globals.css";
+import { Inter as FontSans } from "next/font/google";
+import { cn } from "@/lib/utils";
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <main
+      className={cn(
+        "min-h-screen bg-background font-sans antialiased",
+        fontSans.variable
+      )}
+    >
+      <Component {...pageProps} />
+    </main>
+  );
 }
